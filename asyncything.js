127253e9273932ddfbc0b78fd1a9048ea1539422
saintbaf <saintbaf@gmail.com>
2019-11-21 09:48:52 +0200

    Update asyncything.js

--- asyncything.js
+++ asyncything.js
@@ -29,7 +29,7 @@
     for (i in data.products) {
       for (const [key, value] of Object.entries(data.products[i])) {
         if(value === theCollectionTitle) { 
-          console.log(value);
+          //console.log(value);
           console.log(data.products[i].title);
           console.log(data.products[i].available);
           console.log(data.products[i].handle);
@@ -49,3 +49,60 @@
   
   // Run the thing 
   getProducts();
+
+
+
+// The data that come back look something like this:
+{
+  "products": [
+    {
+      "title": "White Spiderweb Print Bikini",
+      "collections": [
+        { "name": "Accessories", "handle": "accessories" },
+        { "name": "Product Endpoint", "handle": "product-endpoint-tester" }
+      ],
+      "available": true,
+      "handle": "white-spiderweb-print-bikini",
+      "id": 4331660804184,
+      "featured_image": "White_Spiderweb_Bikini2.jpg",
+      "secondary_image": "White_Spiderweb_BikiniTop2.jpg",      
+      "options": [{ "name": "Title", "position": 1, "product_id": 4331660804184 }],
+      "product_type": "Bikinis",
+      "price": 14.99,
+      "price_max": 14.99,
+      "price_min": 14.99,
+      "compare_at_price_max": 0.00,
+      "compare_at_price_min": 0.00,
+      "compare_at_price_varies": false,
+      "tags": ["bikini", "spider", "spiderweb", "web", "white"],
+      "splitTitle": "White Spiderweb Print Bikini",
+      "splitColor": "White Spiderweb Print Bikini",
+      "url": "/products/white-spiderweb-print-bikini",
+      "vendor": "Docblack",
+      "variants": [
+        {
+          "available": true,
+          "id": 31257403752536,         
+          "inventory_policy": "deny",
+          "inventory_quantity": 100,
+          "option1": "Size Free",
+          "option2": "",
+          "option3": "",
+          "position": 1,
+          "price": "1499",
+          "requires_shipping": true,
+          "sku": "DB345-WHT-0",
+          "taxable": true,
+          "title": "Size Free",
+          "weight": 0.0,
+          "weight_unit": "lb"
+        }
+      ]
+    },
+
+    ... ... .. .. . .
+    ... ... .. ..
+    ... ...
+
+
+
